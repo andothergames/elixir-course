b1d74ebdf52441f7f2fe131aa879e0242237a1dd
Joy <32204909+andothergames@users.noreply.github.com>
2024-10-15 16:09:31 +0100

    21: (wip) library fees - dates

---
+++ library-fees/lib/library_fees.ex
@@ -0,0 +1,27 @@
+defmodule LibraryFees do
+  def datetime_from_string(string) do
+    NaiveDateTime.from_iso8601!(string)
+  end
+
+  def before_noon?(datetime) do
+    datetime
+    |> NaiveDateTime.to_time()
+    |> Time.compare(~T[12:00:00]) == :lt
+  end
+
+  def return_date(checkout_datetime) do
+    # Please implement the return_date/1 function
+  end
+
+  def days_late(planned_return_date, actual_return_datetime) do
+    # Please implement the days_late/2 function
+  end
+
+  def monday?(datetime) do
+    # Please implement the monday?/1 function
+  end
+
+  def calculate_late_fee(checkout, return, rate) do
+    # Please implement the calculate_late_fee/3 function
+  end
+end
