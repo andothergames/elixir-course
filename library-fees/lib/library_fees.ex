10709351141a4eda7d21e819d6237f9ca980b97e
Joy <32204909+andothergames@users.noreply.github.com>
2024-11-18 13:06:26 +0000

    21: library fees - date and time

--- library-fees/lib/library_fees.ex
+++ library-fees/lib/library_fees.ex
@@ -10,18 +10,40 @@ defmodule LibraryFees do
   end
 
   def return_date(checkout_datetime) do
-    # Please implement the return_date/1 function
+    days =
+      if before_noon?(checkout_datetime) do
+        28
+      else
+        29
+      end
+
+    checkout_datetime
+    |> NaiveDateTime.add(days * 24 * 60 * 60)
+    |> NaiveDateTime.to_date()
   end
 
   def days_late(planned_return_date, actual_return_datetime) do
-    # Please implement the days_late/2 function
+    actual_return_datetime
+    |> NaiveDateTime.to_date()
+    |> Date.diff(planned_return_date)
+    |> max(0)
   end
 
   def monday?(datetime) do
-    # Please implement the monday?/1 function
+    NaiveDateTime.to_date(datetime)
+    |> Date.day_of_week() == 1
   end
 
   def calculate_late_fee(checkout, return, rate) do
-    # Please implement the calculate_late_fee/3 function
+    return_datetime = datetime_from_string(return)
+
+    days_late =
+      checkout
+      |> datetime_from_string()
+      |> return_date()
+      |> days_late(return_datetime)
+
+    fee = rate * days_late
+    if monday?(return_datetime), do: trunc(fee * 0.5), else: fee
   end
 end
