f08e767d12d245627ae261925324ea9260e8b4e6
andothergames <morrisjoy@ehotmail.co.uk>
2024-11-20 21:55:03 +0000

    9: (2) kitchen calc - pattern match

--- kitchen-calculator/lib/kitchen_calculator.ex
+++ kitchen-calculator/lib/kitchen_calculator.ex
@@ -3,32 +3,53 @@ defmodule KitchenCalculator do
     elem(volume_pair, 1)
   end
 
-  def to_milliliter(volume_pair) do
-    unit = elem(volume_pair, 0)
-    volume = get_volume(volume_pair)
-
-    cond do
-      unit === :cup -> {:milliliter, volume * 240}
-      unit === :fluid_ounce -> {:milliliter, volume * 30}
-      unit === :teaspoon -> {:milliliter, volume * 5}
-      unit === :tablespoon -> {:milliliter, volume * 15}
-      true -> {:milliliter, volume}
-    end
+  def to_milliliter({:cup, volume} = _volume_pair) do
+    {:milliliter, 240 * volume}
   end
 
-  def from_milliliter(volume_pair, unit) do
-    volume = get_volume(volume_pair)
+  def to_milliliter({:fluid_ounce, volume} = _volume_pair) do
+    {:milliliter, 30 * volume}
+  end
 
-    cond do
-      unit === :cup -> {unit, volume / 240}
-      unit === :fluid_ounce -> {unit, volume / 30}
-      unit === :teaspoon -> {unit, volume / 5}
-      unit === :tablespoon -> {unit, volume / 15}
-      true -> {unit, volume}
-    end
+  def to_milliliter({:teaspoon, volume} = _volume_pair) do
+    {:milliliter, 5 * volume}
+  end
+
+  def to_milliliter({:tablespoon, volume} = _volume_pair) do
+    {:milliliter, 15 * volume}
+  end
+
+  def to_milliliter({:milliliter, volume} = _volume_pair) do
+    {:milliliter, volume}
+  end
+
+  def from_milliliter(volume_pair, :cup = _unit) do
+    {_, mls} = to_milliliter(volume_pair)
+    {:cup, mls / 240}
+  end
+
+  def from_milliliter(volume_pair, :fluid_ounce = _unit) do
+    {_, mls} = to_milliliter(volume_pair)
+    {:fluid_ounce, mls / 30}
+  end
+
+  def from_milliliter(volume_pair, :teaspoon = _unit) do
+    {_, mls} = to_milliliter(volume_pair)
+    {:teaspoon, mls / 5}
+  end
+
+  def from_milliliter(volume_pair, :tablespoon = _unit) do
+    {_, mls} = to_milliliter(volume_pair)
+    {:tablespoon, mls / 15}
+  end
+
+  def from_milliliter(volume_pair, :milliliter = _unit) do
+    {_, mls} = to_milliliter(volume_pair)
+    {:milliliter, mls}
   end
 
   def convert(volume_pair, unit) do
-    from_milliliter(to_milliliter(volume_pair), unit)
+    mls = to_milliliter(volume_pair)
+    from_milliliter(mls, unit)
   end
 end
